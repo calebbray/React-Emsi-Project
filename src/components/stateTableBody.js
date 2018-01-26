294a778ed7ab5b6081adfb7b48c05c16a6f7f341
Caleb Bray <caleb.bray@wsu.edu>
2018-01-26 00:20:39 -0800

    Data and numbers correct. Awaiting for deployment

--- src/components/stateTableBody.js
+++ src/components/stateTableBody.js
@@ -6,9 +6,9 @@ class StateTablebody extends Component {
             <tr>
               <td className="left-column">State</td>
               <td className="right-column">{this.props.states[0]}</td>
-              <td className="right-column">{this.props.states[this.props.states.length-1]}</td>
-              <td className="right-column">{(this.props.states[this.props.states.length-1] - this.props.states[0])}</td>
-              <td className="right-column">{((1 - (this.props.states[this.props.states.length-1] / this.props.states[0])) * -100).toFixed(1)}%</td>
+              <td className="right-column">{this.props.states[this.props.states.length-2]}</td>
+              <td className="right-column">{(this.props.states[this.props.states.length-2] - this.props.states[0])}</td>
+              <td className="right-column">{((1 - (this.props.states[this.props.states.length-2] / this.props.states[0])) * -100).toFixed(1)}%</td>
             </tr>
       );
     }
